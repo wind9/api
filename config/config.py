3694fedc232bd88bee32ec6cb2bb80385e12039f
yunwei <yunwei@ruitone.com.cn>
2020-05-22 14:27:18 +0800

    添加fadan ruitone api空文件

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ class Config(object):
     def __init__(self, config_file='config.ini'):
         self._path = os.path.join(os.path.dirname(__file__), 'config.ini')
         if not os.path.exists(self._path):
-            raise FileNotFoundError("can not find file config.ini")
+            raise FileNotFoundError("can not find config.ini")
         self._config = configparser.ConfigParser()
         self._config.read(self._path, encoding='utf-8')
 
